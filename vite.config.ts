293847cbb0840d46f9c41af13461f58f2d7ee45e
mathew_muwomo <116170969+muwomotapiwa@users.noreply.github.com>
2026-05-19 20:05:22 +0200

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // Required for GitHub Pages:
+  // https://muwomotapiwa.github.io/Cee-Hatinators-ZA/
+  base: '/Cee-Hatinators-ZA/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
     },
   },
+
   server: {
     hmr: process.env.DISABLE_HMR !== 'true',
     watch: process.env.DISABLE_HMR === 'true' ? null : {},
